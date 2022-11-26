ca9b7fd84835f096a8e5dda071f92fc3f70a075d
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-27 01:45:36 +0500

    решил задачу 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+void findAllEvenInteger(int size)
+{
+    // Созадние массива с трёхзначными числами размера size (от пользователя)
+    int[] myArray = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        myArray[i] = new Random().Next(100,1000);
+    }
+    
+    // Отображение массива на экране
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{myArray[i] + ", "}");
+    }
+    Console.WriteLine();
+
+    // поиск и сложение чётных чисел
+    int counter = 0; // счётчик чисел
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i] % 2 ==0) counter++;
+    }
+
+    Console.WriteLine($"Кол-во чётных чисел в массиве {counter}");
+}
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+findAllEvenInteger(size);
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
