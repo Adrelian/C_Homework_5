ba33e6a2d10622c2f5cf8023c58e6091e71782a9
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-27 02:02:55 +0500

    решил задачу 2

--- Program.cs
+++ Program.cs
@@ -2,39 +2,75 @@
 
 // [345, 897, 568, 234] -> 2
 
-void findAllEvenInteger(int size)
-{
-    // Созадние массива с трёхзначными числами размера size (от пользователя)
-    int[] myArray = new int [size];
-    for (int i = 0; i < size; i++)
-    {
-        myArray[i] = new Random().Next(100,1000);
-    }
+// void findAllEvenInteger(int size)
+// {
+//     // Созадние массива с трёхзначными числами размера size (от пользователя)
+//     int[] myArray = new int [size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         myArray[i] = new Random().Next(100,1000);
+//     }
     
-    // Отображение массива на экране
-    for (int i = 0; i < size; i++)
-    {
-        Console.Write($"{myArray[i] + ", "}");
-    }
-    Console.WriteLine();
+//     // Отображение массива на экране
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.Write($"{myArray[i] + ", "}");
+//     }
+//     Console.WriteLine();
 
-    // поиск и сложение чётных чисел
-    int counter = 0; // счётчик чисел
-    for (int i = 0; i < myArray.Length; i++)
-    {
-        if (myArray[i] % 2 ==0) counter++;
-    }
+//     // поиск и сложение чётных чисел
+//     int counter = 0; // счётчик чисел
+//     for (int i = 0; i < myArray.Length; i++)
+//     {
+//         if (myArray[i] % 2 ==0) counter++;
+//     }
 
-    Console.WriteLine($"Кол-во чётных чисел в массиве {counter}");
-}
-Console.Write("Введите размер массива: ");
-int size = Convert.ToInt32(Console.ReadLine());
-findAllEvenInteger(size);
+//     Console.WriteLine($"Кол-во чётных чисел в массиве {counter}");
+// }
+// Console.Write("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// findAllEvenInteger(size);
+
+
+
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+void findSumNumberOnOddPosition()
+{
+    // Создаём массив из случайных и случайного размера
+
+    //Задаём случайны размер
+    int size = new Random().Next(5, 10); 
+    int [] myArray = new int [size];
+
+    // Задаём массив
+    Console.Write("Массив -> ");
+    for (int i = 0; i < size; i++)
+    {
+        myArray[i] = new Random().Next(1, 100);
+    }
+    
+    // Отображаем массив
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($" {myArray[i] + ", "} ");
+    }
+    Console.WriteLine();
+
+    // Сумма элементов на чётных ппзициях
+    int sum = 0;
+    for (int i = 1; i < size; i++) // Счёт начинаем с позиции 1
+    {
+        if (i % 2 == 0) sum += myArray[i];
+
+    }
+    Console.WriteLine($"Сумма чётных элементов {sum}");
+}
+
+findSumNumberOnOddPosition();
 
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
